997c24e00a31f07cc195270a390679a2447cb6fa
GiuliannoDevs <127225941+GiuliannoMorales@users.noreply.github.com>
2025-05-14 16:42:57 -0400

    Revert "Agregado router y Layout generales basicos"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
 
-import EstadoCuenta from './pages/EstadoCuenta';
-
 function App() {
-	return (
-		<Router>
-			<Routes>
-				<Route path="/cuenta/estado" element={<EstadoCuenta />} />
-			</Routes>
-		</Router>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
